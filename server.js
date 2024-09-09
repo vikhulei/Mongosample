a45d7975a897b481642619ad37b98cced32949e5
vikhulei <vikhulei@gmail.com>
2024-09-09 06:15:50 +0100

    Routes created

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('API running'))
 
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
+
 app.listen(PORT, () => console.log(`server started on ${PORT}`))
