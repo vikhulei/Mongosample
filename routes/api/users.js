8197d4dc99c43fc596d41fd49b1a1a3e848c1e94
vikhulei <vikhulei@gmail.com>
2024-09-26 07:38:36 +0100

    auth added

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -30,7 +32,7 @@ router.post('/', [
 
 
     if (user) {
-        res.status(400).json({ errors: [{ msg: "User already exists" }] })
+       return res.status(400).json({ errors: [{ msg: "User already exists" }] })
     }
 
 
@@ -58,9 +60,23 @@ router.post('/', [
 
         await user.save()
 
-        //Return jsonwebtoken 
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-        res.send('User registered')
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+            if(err) throw err
+            res.json( { token })
+        })
+
+
+        // res.send('User registered')
 
 
 
